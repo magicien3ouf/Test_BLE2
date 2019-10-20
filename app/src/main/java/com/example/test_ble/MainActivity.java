6cae50a052b943b0bab10a8b27cd02963a89bb66
magicien3ouf <50133495+magicien3ouf@users.noreply.github.com>
2019-10-21 00:43:06 +0200

    Test si le tel est compatible BT, si oui, demande l'activation et l'active.
    Affiche le nom et l'addresse MAC de l'objet BT connecté.

--- app/src/main/java/com/example/test_ble/MainActivity.java
+++ app/src/main/java/com/example/test_ble/MainActivity.java
@@ -2,14 +2,53 @@ package com.example.test_ble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+                helloTextView.setText("Name of the connected device : " + deviceName + "\nMAC address of the connected device : \n" + deviceHardwareAddress);
+
+
+            }
+
+        }
+
+
+
+
+
     }
 }
+
+
